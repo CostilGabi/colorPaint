5904e3e6c3c322deaf4bd3f08c138cc3bc1fe8e0
Costil Gabriel <37557421+CostilGabi@users.noreply.github.com>
2023-07-08 14:22:29 +0300

    Main commint

---
+++ src/App.tsx
@@ -0,0 +1,124 @@
+import { useEffect, useState } from 'react'
+import Canvas from './Canvas'
+import { ReactComponent as Delete } from './assets/delete.svg'
+
+function App() {
+    const [deleteMode, setDeleteMode] = useState(false)
+    const [numWidth, setNumWidth] = useState(2)
+    const [shadoWidth, setShadoWidth] = useState(15)
+    const [shadowColor, setShadowColor] = useState('yellow')
+    const [lineColor, setLineColor] = useState('white')
+
+    const handleDeleteMode = () => {
+        setDeleteMode( (prevDeleteMode) => !prevDeleteMode )        
+    }
+
+    const handleNumWidthPlus = () => {
+        setNumWidth( numWidth + 1 )
+    }
+
+    const handleNumWidthMinus= () => {
+        
+        if ( numWidth != 1 ) {
+            setNumWidth( numWidth - 1 )
+        }
+    }
+
+    const handleShadoWidthPlus = () => {
+        setShadoWidth( shadoWidth + 2 )
+    }
+
+    const handleShadoWidthMinus= () => {
+        
+        if ( shadoWidth != 1 ) {
+            setShadoWidth( shadoWidth - 2 )
+        }
+    }
+
+    const handleShadowColor = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setShadowColor( event.target.value )
+    }
+
+    const handleLineColor = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setLineColor( event.target.value )
+    }
+
+    const shadowColorStyle = {
+        backgroundColor: shadowColor,
+        width: '20px',
+        height: '20px',
+        borderRadius: '50%'
+    }
+
+    const lineColorStyle = {
+        backgroundColor: lineColor,
+        width: '20px',
+        height: '20px',
+        borderRadius: '50%'
+    }
+
+    useEffect(() => {
+        setDeleteMode(false)
+    }, [deleteMode])
+
+    useEffect(() => {
+        setNumWidth(numWidth)
+    }, [numWidth])
+
+    return (
+
+        <div className='colorpaint'>
+
+            <div className='colorpaint-main'>
+
+                <div className='colorpaint-main-controls'>
+                
+                    <div className='colorpaint-main-controls-width'>
+                        <span>Line width</span>
+                        <input type='text' readOnly value={numWidth} />
+                        <div className='colorpaint-main-controls-width-c'>
+                            <button onClick={handleNumWidthMinus}>-</button>
+                            <button onClick={handleNumWidthPlus}>+</button>
+                        </div>
+                    </div>
+
+                    <div className='colorpaint-main-controls-width'>
+                        <span>Shadow width</span>
+                        <input type='text' readOnly value={shadoWidth} />
+                        <div className='colorpaint-main-controls-width-c'>
+                            <button onClick={handleShadoWidthMinus}>-</button>
+                            <button onClick={handleShadoWidthPlus}>+</button>
+                        </div>
+                    </div>
+
+                    <div className='colorpaint-main-controls-color'>
+                        <span>Shadow Color (hex or name) </span>
+                        <input type='text' value={shadowColor} onChange={handleShadowColor} />
+                        <div className='colorpaint-main-controls-sc-bubble' style={shadowColorStyle}></div>
+                    </div>
+
+                    <div className='colorpaint-main-controls-color'>
+                        <span>Line Color (hex or name) </span>
+                        <input type='text' value={lineColor} onChange={handleLineColor} />
+                        <div className='colorpaint-main-controls-sc-bubble' style={lineColorStyle}></div>
+                    </div>
+
+                    <button onClick={handleDeleteMode} type='button' className='colorpaint-main-controls-delete'>
+                        <Delete />
+                        <span>Erase</span>
+                    </button>
+
+                </div>
+
+                <div className='colorpaint-main-board'>
+                    <Canvas thickMode={numWidth} shadowMode={shadoWidth} shadowColor={shadowColor} lineColor={lineColor} deleteMode={deleteMode} />
+                </div>
+
+            </div>
+
+
+        </div>
+    )
+}
+
+export default App
---
+++ src/Canvas.tsx
@@ -0,0 +1,116 @@
+import { useRef, useEffect } from 'react'
+
+type CanvasProps = {
+    deleteMode: boolean,
+    thickMode: number
+    shadowMode: number
+    shadowColor: string
+    lineColor: string
+}
+
+const Canvas: React.FC<CanvasProps> = ({ deleteMode, thickMode, shadowMode, shadowColor, lineColor }) => {
+    const canvasRef = useRef<HTMLCanvasElement>(null)
+    const isDrawing = useRef(false)
+
+    const startDrawing = () => {
+        isDrawing.current = true
+    }
+
+    const stopDrawing = (ctx: CanvasRenderingContext2D) => {
+        isDrawing.current = false
+        ctx?.beginPath()
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    const letDraw = (ctx: CanvasRenderingContext2D, x: number, y: number) => {
+
+        if ( !isDrawing.current ) return
+
+        ctx.lineWidth = thickMode
+        ctx.lineCap = 'round'
+        ctx.strokeStyle = lineColor
+        ctx.shadowColor = shadowColor
+        ctx.shadowBlur = shadowMode
+        ctx.lineTo(x, y)
+        ctx.stroke()
+        ctx.beginPath() // Start a new path
+        ctx.moveTo(x, y)
+    
+    }
+
+    useEffect(() => {
+        const canvas = canvasRef.current
+        if (!canvas) return;
+
+        const context = canvas.getContext('2d');
+        if (!context) return;
+
+        let animationFrameId: number
+
+        const handleMouseMove = (e: MouseEvent) => {
+
+            if (!canvas || !context) return
+
+            const canvasRect = canvas.getBoundingClientRect()
+            const offsetX = e.clientX - canvasRect.left
+            const offsetY = e.clientY - canvasRect.top
+
+            letDraw(context, offsetX, offsetY)
+
+        }
+
+        const handleMouseDown = () => {
+            startDrawing()
+        }
+
+        const handleMouseUp = () => {
+            stopDrawing(context)
+        }
+
+        const deleteBoard = () => {
+            context.clearRect(0, 0, canvas.width, canvas.height);
+        }
+      
+        if (deleteMode) {
+            deleteBoard()
+        }
+
+        canvas.addEventListener('mousemove', handleMouseMove)
+        canvas.addEventListener('mousedown', handleMouseDown)
+        canvas.addEventListener('mouseup', handleMouseUp)
+
+        return () => {
+            canvas.removeEventListener('mousemove', handleMouseMove);
+            canvas.removeEventListener('mousedown', handleMouseDown);
+            canvas.removeEventListener('mouseup', handleMouseUp);
+            cancelAnimationFrame(animationFrameId);
+        }
+    }, [deleteMode, letDraw])
+
+    useEffect(() => {
+        const resizeCanvas = () => {
+            const canvas = canvasRef.current;
+            if (!canvas) return;
+    
+            const parent = canvas.parentElement;
+            if (!parent) return;
+    
+            const { width, height } = parent.getBoundingClientRect();
+            canvas.width = width;
+            canvas.height = height;
+        }
+    
+        window.addEventListener('resize', resizeCanvas);
+        resizeCanvas();
+    
+        return () => {
+            window.removeEventListener('resize', resizeCanvas);
+        }
+      }, [])
+
+    return (
+        <canvas ref={canvasRef} style={{ width: '100%', height: '100%' }} />
+    )
+}
+
+export default Canvas
